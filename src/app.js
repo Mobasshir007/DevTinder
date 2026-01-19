bdd290785776bebb9a9a7c61ea6b55c0b0d3a6d7
Mobasshir <mobasshirhussain2002@gmail.com>
2026-01-20 01:23:32 +0530

    cmt

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ app.listen(7777)
 app.use(("/test"),function(req,res){
     res.send("hello! server is running on port 7777")
 })
-app.get(("/user"),(req,res)=>{
+app.get(("/user/:userId/:name/:pass"),(req,res)=>{
+     console.log(req.params)
     res.send({firstName:"Mobasshir",lastName:"Hussain"})
 })
 app.post(("/user"),(req,res)=>{
+   
     // saving data in DB
     res.send("Data saved successfully")
 })
